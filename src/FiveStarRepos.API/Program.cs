f5864a357ce8c0a5b1adfd8a97ae96c725bcca18
Mateus Candido <mateus.candido@live.com>
2023-02-08 02:49:05 -0300

    init

---
+++ src/FiveStarRepos.API/Program.cs
@@ -0,0 +1,18 @@
+using FiveStarRepos.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace asda
+{
+    public class Program
+    {
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/FiveStarRepos.Infra/IoC/DependencyResolver.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FiveStarRepos.Infra.IoC
+{
+    public static class DependencyResolver
+    {
+        public static IServiceCollection ConfigureApplicationContext(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
